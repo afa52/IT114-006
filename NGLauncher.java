eb7a76f48845232e4c1de522c44aff69e85c2bdc
afa52 <afa52@njit.edu>
2023-02-15 15:57:25 -0500

    base files

---
+++ NGLauncher.java
@@ -0,0 +1,7 @@
+//Added during recording to demonstrate access modifiers in practice
+public class NGLauncher {
+    public static void main(String[] args) {
+        NumberGuesser4 ng = new NumberGuesser4();
+        ng.start();
+    }
+}
