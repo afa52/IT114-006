fba110824af4ee2a54a5ba2bbb66c0c3f1260610
afa52 <afa52@njit.edu>
2023-02-15 19:47:44 -0500

    completed hw

--- NumberGuesser4.java
+++ NumberGuesser4.java
@@ -14,65 +14,67 @@ public class NumberGuesser4 {
     private boolean pickNewRandom = true;
     private Random random = new Random();
     private String fileName = "ng4.txt";
-    private String[] fileHeaders = {"Level", "Strikes", "Number", "MaxLevel"};//used for demo readability
+    private String[] fileHeaders = { "Level", "Strikes", "Number", "MaxLevel" };// used for demo readability
 
-    private void saveState(){
-        String[] data = {level+"", strikes+"", number +"", maxLevel+""};
+    private void saveState() {
+        String[] data = { level + "", strikes + "", number + "", maxLevel + "" };
         String output = String.join(",", data);
-        //Note: we don't need a file reference as FileWriter creates the file if it doesn't exist
+        // Note: we don't need a file reference as FileWriter creates the file if it
+        // doesn't exist
         try (FileWriter fw = new FileWriter(fileName)) {
-			fw.write(String.join(",", fileHeaders));
-            fw.write("\n");//new line
+            fw.write(String.join(",", fileHeaders));
+            fw.write("\n");// new line
             fw.write(output);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
-    private void loadState(){
+
+    private void loadState() {
         File file = new File(fileName);
-		if (!file.exists()) {
-            //Not providing output here as it's expected for a fresh start
-			return;
-		}
-		try (Scanner reader = new Scanner(file)) {
+        if (!file.exists()) {
+            // Not providing output here as it's expected for a fresh start
+            return;
+        }
+        try (Scanner reader = new Scanner(file)) {
             int lineNumber = 0;
-			while (reader.hasNextLine()) {
-				String text = reader.nextLine();
-                //System.out.println("Text: " + text);
-                if(lineNumber == 1){
+            while (reader.hasNextLine()) {
+                String text = reader.nextLine();
+                // System.out.println("Text: " + text);
+                if (lineNumber == 1) {
                     String[] data = text.split(",");
                     String level = data[0];
                     String strikes = data[1];
                     String number = data[2];
                     String maxLevel = data[3];
                     int temp = strToNum(level);
-                    if(temp > -1){
+                    if (temp > -1) {
                         this.level = temp;
                     }
                     temp = strToNum(strikes);
-                    if(temp > -1){
+                    if (temp > -1) {
                         this.strikes = temp;
                     }
-                    temp=strToNum(number);
-                    if(temp > -1){
+                    temp = strToNum(number);
+                    if (temp > -1) {
                         this.number = temp;
                         pickNewRandom = false;
                     }
                     temp = strToNum(maxLevel);
-                    if(temp > -1){
+                    if (temp > -1) {
                         this.maxLevel = temp;
                     }
                 }
                 lineNumber++;
-			}
-		} catch (FileNotFoundException e) {//specific exception
-			e.printStackTrace();
-		} catch (Exception e2) {//any other unhandled exception
-			e2.printStackTrace();
-		}
+            }
+        } catch (FileNotFoundException e) {// specific exception
+            e.printStackTrace();
+        } catch (Exception e2) {// any other unhandled exception
+            e2.printStackTrace();
+        }
         System.out.println("Loaded state");
-	int range = 10 + ((level - 1) * 5);
+        int range = 10 + ((level - 1) * 5);
         System.out.println("Welcome to level " + level);
         System.out.println(
                 "I picked a random number between 1-" + (range) + ", let's see if you can guess.");
@@ -104,8 +106,44 @@ public class NumberGuesser4 {
             System.out.println("Tired of playing? No problem, see you next time.");
             processed = true;
         }
-        //TODO add other conditions here
+        // TODO add other conditions here
         return processed;
+    }
+
+    /**************************************************************************
+     * afa52
+     * 2/15/23
+     * Create a method called higherLower that passes in guess
+     * as a parameter and then use a boolean condition that checks if guess
+     * is greater than the number generated, if the condition is true then print
+     * to the user "Hint: Guess Lower" and then an if-else statement that
+     * checks if guess is less than the number generated and if that condition
+     * is true then print to the user "Hint: Guess Higher"
+     **************************************************************************/
+    private void higherLower(int guess) {
+        if (guess > number)
+            System.out.println("(Hint: Guess Lower)");
+        else if (guess < number)
+            System.out.println("(Hint: Guess Higher)");
+    }
+
+    /**************************************************************************
+     * afa52
+     * 2/15/23
+     * Create a method called hotCold that passes in guess
+     * as a parameter and then use a boolean condition that will take the
+     * absolute value of the user's guess (guess) minus the computer generated 
+     * vale (number). If the equation is less than the 5, then the user's guess
+     * is warm, if the equation is less than or equal to 3, then the user's guess
+     * is hot, otherwise the user's guess is cold. 
+     **************************************************************************/
+    private void hotCold(int guess) {
+        if (Math.abs(guess - number) <= 5)
+            System.out.println("Warm");
+        else if (Math.abs(guess - number) <= 3)
+            System.out.println("Hot");
+        else
+            System.out.println("Cold");
     }
 
     private void lose() {
@@ -128,6 +166,8 @@ public class NumberGuesser4 {
             pickNewRandom = true;
         } else {
             System.out.println("That's wrong");
+            higherLower(guess);
+            hotCold(guess);
             strikes++;
             if (strikes >= maxStrikes) {
                 lose();
@@ -142,7 +182,7 @@ public class NumberGuesser4 {
             guess = Integer.parseInt(message.trim());
         } catch (NumberFormatException e) {
             System.out.println("You didn't enter a number, please try again");
-        }catch(Exception e2){
+        } catch (Exception e2) {
             System.out.println("Null message received");
         }
         return guess;
@@ -160,19 +200,20 @@ public class NumberGuesser4 {
                     pickNewRandom = false;
                 }
                 System.out.println("Type a number and press enter");
-                //we'll want to use a local variable here
-                //so we can feed it into multiple functions
+                // we'll want to use a local variable here
+                // so we can feed it into multiple functions
                 String message = input.nextLine();
                 // early termination check
                 if (processCommands(message)) {
-                    //command handled; don't proceed with game logic
+                    // command handled; don't proceed with game logic
                     break;
                 }
-                //this is just to demonstrate we can return a value and pass it into another method
+                // this is just to demonstrate we can return a value and pass it into another
+                // method
                 int guess = strToNum(message);
                 processGuess(guess);
-                //the following line is the same as the above two lines
-                //processGuess(getGuess(message));
+                // the following line is the same as the above two lines
+                // processGuess(getGuess(message));
             } while (true);
         } catch (Exception e) {
             System.out.println("An unexpected error occurred. Goodbye.");
