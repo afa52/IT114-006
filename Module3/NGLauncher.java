b9734f65258b534e9387e0bee9137c9407e66d00
afa52 <afa52@njit.edu>
2023-03-05 17:03:11 -0500

    updating files

--- Module3/NGLauncher.java
+++ Module3/NGLauncher.java
@@ -1,3 +1,4 @@
+package Module3;
 //Added during recording to demonstrate access modifiers in practice
 public class NGLauncher {
     public static void main(String[] args) {
--- Module3/NumberGuesser4.java
+++ Module3/NumberGuesser4.java
@@ -1,3 +1,4 @@
+package Module3;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
